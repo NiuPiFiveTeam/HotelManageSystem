305fd2ad66ed5c0e14dcc47d71065da4ea2a39a5
huangxinjian <924168702@qq.com>
2018-09-20 15:56:28 +0800

    日志示例，可以参考使用

--- src/main/java/com/hmm/room/controller/RoomController.java
+++ src/main/java/com/hmm/room/controller/RoomController.java
@@ -1,7 +1,23 @@
 package com.hmm.room.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RoomController {
 	//到此一游
 	//到此一游+1
 	//到此一游+2
+	private Logger logger = LoggerFactory.getLogger(this.getClass()); 
+	
+	
+	@RequestMapping("/room")
+	public String getRoom() {
+		logger.info("nihao");
+		return "room";
+	}
+	
+	
 }
