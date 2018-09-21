30479a0d1458a0b320c277a17dec94bef67f5d51
DJDU <mrdjdu@qq.com>
2018-09-21 09:09:12 +0800

    入库总表和入库详细表

---
+++ src/main/java/com/hmm/stock/entity/InAll.java
@@ -0,0 +1,76 @@
+package com.hmm.stock.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hmm.employee.domain.Employee;
+
+
+
+@Entity
+@Table(name="t_in_All")
+public class InAll {
+	private String inallid;//入库单编号,inAllId+16位随机数
+	private Date date;//入库日期，例:2018/9/16
+	private String vender;//采购商家，例：DJDU有限公司
+	private float inallsum;//采购总金额
+	private List<InDetailed> indetaileds = new ArrayList<InDetailed>();//子详细入库记录
+	private Employee employee;//父入库员工
+	
+	
+	@Id
+	public String getInAllId() {
+		return inallid;
+	}
+	@JsonFormat(pattern="yyyy/MM/dd HH:mm:ss",timezone="GMT+8")
+	public Date getDate() {
+		return date;
+	}
+	public String getVender() {
+		return vender;
+	}
+	public float getInAllSum() {
+		return inallsum;
+	}
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="inAll",fetch=FetchType.LAZY)
+	public List<InDetailed> getInDetaileds() {
+		return indetaileds;
+	}
+	@ManyToOne
+	public Employee getEmployee() {
+		return employee;
+	}
+	
+	
+	public void setInAllId(String inAllId) {
+		this.inallid = inAllId;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public void setVender(String vender) {
+		this.vender = vender;
+	}
+	public void setInAllSum(float inAllSum) {
+		this.inallsum = inAllSum;
+	}
+	public void setInDetaileds(List<InDetailed> inDetaileds) {
+		this.indetaileds = inDetaileds;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+
+
+}
---
+++ src/main/java/com/hmm/stock/entity/InDetailed.java
@@ -0,0 +1,73 @@
+package com.hmm.stock.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="t_in_detailed")
+public class InDetailed {
+	private int id;
+	private String goodsName;//商品名，例：牙刷
+	private String unit;//单位，例：只
+	private float price;//单价，例：10元/只
+	private float amount;//数量，例:100只
+	private InAll inAll;//父入库记录
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	@ManyToOne//多对一级联删除，使用ALL建议先移除关系（update）再delete
+	public InAll getInAll() {
+		return inAll;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public void setInAll(InAll inAll) {
+		this.inAll = inAll;
+	}
+	
+	
+
+	
+	
+	
+}
