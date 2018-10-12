27411a40e1cbc8d4147b0f0bd0bdddfa45a15bc0
DJDU <mrdjdu@qq.com>
2018-10-12 16:53:22 +0800

    1

--- src/main/java/com/hmm/logistics/roomClean/controller/RoomCleanController.java
+++ src/main/java/com/hmm/logistics/roomClean/controller/RoomCleanController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hmm.common.beans.BeanUtils;
@@ -59,7 +60,8 @@ public class RoomCleanController {
 		}
 	}
 	@RequestMapping("/changeRoomState")
-	public ExtAjaxResponse changeRoomState() {
+	public String changeRoomState(@RequestParam("roomNo") String roomNo,@RequestParam("selectValue") String selectValue) {
+		System.out.println(roomNo+" "+selectValue);
 		return null;
 	
 	}
