a80c6251803b18de7a359cb602904a02fe02c603
huangxinjian <924168702@qq.com>
2018-10-23 20:39:57 +0800

    上交两个东西删除一个东西

--- admin-dashboard/classic/src/view/room/allroom/AllRoom.js
+++ admin-dashboard/classic/src/view/room/allroom/AllRoom.js
@@ -1,11 +1,12 @@
 Ext.define('Admin.view.room.allroom.AllRoom', {
     extend: 'Ext.container.Container',
     xtype: 'allRoom',
+    id:'allRoomContainer',
     // requires: ['Ext.tree.Panel'],
     //controller: 'order',				//viewController:代码与视图分离。声明视图绑定的事件，可以多个视图共享。
     //viewModel: {type: 'orderlist'},	//viewModel：配置Stote数据源。多个视图共享Store。
     
-    // controller: 'inroomViewController',   //事件控制器，用来编写事件
+    controller: 'allRoomViewController',   //事件控制器，用来编写事件
     // viewModel : {type: 'inRoomViewModel'},	
     //用来管理数据
    
